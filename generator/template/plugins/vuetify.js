a8e3f0ba9341b9a07df524935df3503795a193c7
Kami -san <p_d.e.konoval@mpt.ru>
2020-03-11 15:42:32 +0300

    Vuetify configuration

---
+++ generator/template/plugins/vuetify.js
@@ -0,0 +1,28 @@
+import ru from 'vuetify/es5/locale/ru';
+
+/**
+ * Конфигурация Vuetify
+ */
+export default {
+  theme: {
+    themes: {
+      light: {
+        primary: '#2e3e4f',
+        secondary: '#5227b4',
+        accent: '#cfd108',
+        warning: '#fe891f',
+
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+      },
+    },
+  },
+    lang: {
+      locales: { ru },
+      current: 'ru',
+    },
+  icons: {
+    iconfont: 'mdi',
+  },
+};
