ef4db8f1d55b7caafee114051c1451eedd91e8fe
Kami -san <1.dankon.1@gmail.com>
2020-03-19 16:47:07 +0300

    Перенёс на директорию выше всё

--- generator/index.js
+++ generator/index.js
@@ -1,16 +1,5 @@
 const generator = (api, options, preset) => {
-  api.render('./template/components/base');
-  api.render('./template/components/helper');
-  api.render('./template/components/layout');
-
-  // if (options.charts) {
-  //   api.extendPackage({
-  //     dependencies: {
-  //       'chart.js': '^2.9.3',
-  //     },
-  //   });
-  //   api.render('./template/components/charts');
-  // }
+  api.render('./template');
 };
 
 module.exports = generator;
