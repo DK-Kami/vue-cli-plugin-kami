5647fd8d1ccfddd63e70d22c33287402a9618134
Kami -san <1.dankon.1@gmail.com>
2020-03-19 17:48:47 +0300

    Добавление зависимостей

--- generator/index.js
+++ generator/index.js
@@ -1,5 +1,23 @@
 const generator = (api, options, preset) => {
   api.render('./template');
+
+  api.extendPackage({
+    depencies: {
+      "axios": "^0.19.0",
+      "chart.js": "^2.9.3",
+      "vue-router": "^3.1.3",
+      "vue-the-mask": "^0.11.1",
+      "vuetify": "^2.1.0",
+      "vuex": "^3.1.2"
+    },
+    devDepencies: {
+      "@mdi/font": "^4.7.95",
+      "@vue/cli-plugin-router": "^4.2.0",
+      "@vue/cli-plugin-vuex": "^4.2.0",
+      "vue-cli-plugin-vuetify": "^2.0.2",
+      "vuetify-loader": "^1.3.0",
+    }
+  })
 };
 
 module.exports = generator;
