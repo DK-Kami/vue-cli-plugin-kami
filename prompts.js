1169d2f5579b18a196ec64927f6d5c4fa72eedd3
Kami <1.dankon.1@gmail.com>
2020-03-21 21:51:43 +0300

    test prompts

---
+++ prompts.js
@@ -0,0 +1,96 @@
+const prompts = [
+  {
+    type: 'confirm',
+    name: 'pidor',
+    message: 'Пидор?',
+  },
+  {
+    type: 'checkbox',
+    name: 'hui',
+    message: 'Хуй соссать нравится?',
+    default: 'yes'
+  },
+  {
+    type: 'input',
+    name: 'input',
+    message: 'input',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+  {
+    type: 'number',
+    name: 'number',
+    message: 'number',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+  {
+    type: 'confirm',
+    name: 'confirm',
+    message: 'confirm',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+  {
+    type: 'list',
+    name: 'list',
+    message: 'list',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+  {
+    type: 'rawlist',
+    name: 'rawlist',
+    message: 'rawlist',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+  {
+    type: 'expand',
+    name: 'expand',
+    message: 'expand',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+  {
+    type: 'checkbox',
+    name: 'checkbox',
+    message: 'checkbox',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+  {
+    type: 'password',
+    name: 'password',
+    message: 'password',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+  {
+    type: 'editor',
+    name: 'editor',
+    message: 'editor',
+    choices: [
+      'hui',
+      'pidor',
+    ],
+  },
+];
+
+module.exports = prompts;
