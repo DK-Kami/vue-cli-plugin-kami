6a0eb6a60a545c45c5d6bc7a18e4ba03e9d999ae
Kami <1.dankon.1@gmail.com>
2020-03-26 15:56:06 +0300

    only render

--- generator/index.js
+++ generator/index.js
@@ -1,25 +1,25 @@
 const generator = (api, options, preset) => {
-  console.log('test');
-  console.log(options);
+  // console.log('test');
+  // console.log(options);
   api.render('./template');
 
-  api.extendPackage({
-    dependencies: {
-      "axios": "^0.19.0",
-      "chart.js": "^2.9.3",
-      "vue-router": "^3.1.3",
-      "vue-the-mask": "^0.11.1",
-      "vuetify": "^2.1.0",
-      "vuex": "^3.1.2"
-    },
-    devDependencies: {
-      "@mdi/font": "^4.7.95",
-      "@vue/cli-plugin-router": "^4.2.0",
-      "@vue/cli-plugin-vuex": "^4.2.0",
-      "vue-cli-plugin-vuetify": "^2.0.2",
-      "vuetify-loader": "^1.3.0",
-    }
-  })
+  // api.extendPackage({
+  //   dependencies: {
+  //     "axios": "^0.19.0",
+  //     "chart.js": "^2.9.3",
+  //     "vue-router": "^3.1.3",
+  //     "vue-the-mask": "^0.11.1",
+  //     "vuetify": "^2.1.0",
+  //     "vuex": "^3.1.2"
+  //   },
+  //   devDependencies: {
+  //     "@mdi/font": "^4.7.95",
+  //     "@vue/cli-plugin-router": "^4.2.0",
+  //     "@vue/cli-plugin-vuex": "^4.2.0",
+  //     "vue-cli-plugin-vuetify": "^2.0.2",
+  //     "vuetify-loader": "^1.3.0",
+  //   }
+  // })
 };
 
 module.exports = generator;
