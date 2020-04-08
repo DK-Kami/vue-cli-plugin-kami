20e2b001f2d9066e33d5a31c41f79dd876961fb2
Kami <1.dankon.1@gmail.com>
2020-04-08 16:43:59 +0300

    Update component

--- generator/template/src/components/base/FilteredTable/TableActions.vue
+++ generator/template/src/components/base/FilteredTable/TableActions.vue
@@ -4,11 +4,11 @@
       <v-divider />
     </v-flex>
 
-    <v-flex shrink>
+    <!-- <v-flex shrink>
       <v-layout v-if="headers">
         <footer-table :headers.sync="headers" />
       </v-layout>
-    </v-flex>
+    </v-flex> -->
 
     <v-flex grow>
       <slot name="actions" />
@@ -16,38 +16,37 @@
 
     <v-flex shrink>
       <v-layout class="caption grey--text" justify-end align-center>
-        <template v-if="withSelect">
+        <!-- <template v-if="withSelect">
           <v-flex shrink class="mr-4 py-0 pr-3">Строк на странице:</v-flex>
-
           <v-flex shrink class="py-0">
             <v-select
-              v-model="options.itemsPerPage"
-              :items="itemsPerPageCurrent"
-              class="caption mr-1 pa-0 ma-0 mt-2"
-              style="width: 55px;"
+              v-model="pagination.rowsPerPage"
+              :items="rowsPerPageCurrent"
               item-value="value"
               item-text="text"
+              class="caption mr-1 pa-0 ma-0 mt-2"
+              style="width: 45px;"
             />
           </v-flex>
-          
+
           <v-flex shrink class="py-0 mx-4 px-2">
-            {{ ((options.page - 1) * options.itemsPerPage) + 1 }}-{{ currentPageItems }} из {{ options.itemsLength }} 
+            {{ ((pagination.page - 1) * pagination.rowsPerPage) + 1 }}-{{ currentPageItems }} из {{ pagination.totalItems }} 
           </v-flex>
-        </template>
+        </template> -->
 
         <v-flex shrink class="py-0">
           <v-btn
             :disabled="!mayPrevPage"
-            class="grey--text text--darken-1 mr-1"
             icon
+            class="grey--text text--darken-1 mr-1"
             @click="prevPage"
           >
-            <v-icon>mdi-chevron-left</v-icon>
+            <v-icon>chevron_left</v-icon>
           </v-btn>
         </v-flex>
 
         <v-flex v-if="!withSelect" shrink class="py-0 mx-4 px-2">
-          {{ ((options.page - 1) * options.itemsPerPage) + 1 }}-{{ currentPageItems }} из {{ options.itemsLength }} 
+          {{ ((page - 1) * itemsPerPage) + 1 }}-{{ currentPageItems }} из {{ totalItems }} 
         </v-flex>
 
         <v-flex shrink class="py-0">
@@ -57,7 +56,7 @@
             class="grey--text text--darken-1"
             @click="nextPage"
           >
-            <v-icon>mdi-chevron-right</v-icon>
+            <v-icon>chevron_right</v-icon>
           </v-btn>
         </v-flex>
       </v-layout>
@@ -76,65 +75,45 @@ export default {
   name: 'TableActions',
 
   components: {
-    FooterTable,
+    FooterTable
   },
 
   props: {
-    headers: [Array, Boolean],
+    itemsPerPage: Number,
     withSelect: Boolean,
-    itemsPerPage: Array,
-    itemsCount: Number,
-
-    options: {
-      type: Object,
-      default: () => ({
-        itemsPerPage: Number,
-        itemsLength: Number,
-        page: Number,
-      })
-    }
+    totalItems: Number,
+    page: Number,
   },
 
   computed: {
-    itemsPerPageCurrent() {
-      return this.itemsPerPage.map(r => ({
-        value: r.value || r,
-        text: r.text || r,
-      }));
+    rowsPerPageCurrent() {
+      return [];
     },
     currentPageItems() {
-      const page = this.options.page * this.options.itemsPerPage;
-      return page > this.options.itemsLength
-        ? this.options.itemsLength
+      const page = this.page * this.itemsPerPage;
+      return page > this.totalItems
+        ? this.totalItems
         : page;
     },
     mayPrevPage() {
-      return this.options.page !== 1;
+      return this.page !== 1;
     },
     mayNextPage() {
-      return this.currentPageItems < this.options.itemsLength;
+      return this.currentPageItems < this.totalItems;
     },
   },
 
   methods: {
     prevPage() {
       if (this.mayPrevPage) {
-        this.options.page--;
-        this.$emit('update:options', this.options);
+        this.$emit('update:page', this.page - 1);
       }
     },
     nextPage() {
       if (this.mayNextPage) {
-        this.options.page++;
-        this.$emit('update:options', this.options);
+        this.$emit('update:page', this.page + 1);
       }
     },
-  },
-
-  watch: {
-    itemsCount(newVal) {
-      this.options.itemsLength = newVal;
-    }
   },
 };
 </script>
