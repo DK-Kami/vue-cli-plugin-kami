67fc824dfe83b08a8f2052f555ba889350911aa8
Kami San <1.dankon.1@gmail.com>
2020-08-19 18:43:11 +0300

    delete vuetify config

--- generator/template/main.js
+++ generator/template/main.js
@@ -2,14 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import WebClient from './middleware/WebClient';
-import RStore from './helper/RStore';
 import routerInit from './router';
 import store from './store';
 
 import mainMixin from '@/helper/mixins/main';
-
-import vuetify from './plugins/vuetify';
-import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false
 
@@ -19,16 +15,9 @@ const router = routerInit(store);
 WebClient.router = router;
 WebClient.store = store;
 
-//// Загрузка данных из локального хранилища
-// RStore.subscribe('afterUpdate', state => {
-//   store.dispatch(state ? 'auth/loginFromState' : 'unsetUserData', state);
-//   if (state) router.push('/ping');
-// });
-
 Vue.mixin(mainMixin);
 
 new Vue({
-  vuetify,
   router,
   store,
   render: h => h(App),
