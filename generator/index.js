8596eabcb8ebcbbaa38e1b92bab5dfde0ea4df38
Kami San <1.dankon.1@gmail.com>
2020-08-19 18:40:59 +0300

    delete vuetify dependecieses

--- generator/index.js
+++ generator/index.js
@@ -5,15 +5,11 @@ const generator = (api, options = {}, preset) => {
       "chart.js": "^2.9.3",
       "vue-router": "^3.1.3",
       "vue-the-mask": "^0.11.1",
-      "vuetify": "^2.1.0",
       "vuex": "^3.1.2"
     },
     devDependencies: {
-      "@mdi/font": "^4.7.95",
       "@vue/cli-plugin-router": "^4.2.0",
       "@vue/cli-plugin-vuex": "^4.2.0",
-      "vue-cli-plugin-vuetify": "^2.0.2",
-      "vuetify-loader": "^1.3.0",
     }
   });
 
